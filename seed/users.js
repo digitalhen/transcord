76ad01798276fa1ccfc63693cd54a58de290f6c7
Ubuntu <henry@digitalhen.digitalhen-p43f7701.b9.internal.cloudapp.net>
2018-06-28 17:31:52 +0000

    updated seed
    Former-commit-id: 82c7f4992d9f33bb3c3ef7315a99c2e4b45c2674

--- seed/users.js
+++ seed/users.js
@@ -4,6 +4,7 @@ const users = [
   {name: 'Henry Williams', email: 'recording@digitalhen.com', phoneNumber: '+14156694559'},
   {name: 'Terry Chapman', email: 'blahblah@digitalhen.com', phoneNumber: '+12124442796'},
   {name: 'Joe Blogs', email: 'joeblogs@digitalhen.com', phoneNumber: '+15014442720'},
+  {name: 'Jennifer Yee', email: 'jyee16@gmail.com', phoneNumber: '+19176502722'},
 ];
 
 db.users.insert(users);
