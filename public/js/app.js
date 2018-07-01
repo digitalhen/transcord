6afe3a35033666ebf4a534398bb7f139d26adb77
Henry Williams <bitbucket@digitalhen.com>
2018-07-01 14:38:24 -0400

    Adds profile validation
    Former-commit-id: a72f63753945478c146c0fd07bd1b10f35ffa82d

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,12 @@ $(document).ready(function(){
          && value.length > 7 // is 8 char or longer
   });
 
+  // lets me check if they haven't changed the value
+  $.validator.addMethod("notEqual", function(value, element, param) {
+    return this.optional(element) || value != param;
+    // TODO: the form still needs to be valid
+  });
+
   var loginValidator = $('#form-login').validate({
     rules: {
       username: "required",
@@ -19,6 +25,75 @@ $(document).ready(function(){
     messages: {
       username: "Required",
       password: "Please enter a password."
+    }
+});
+
+  var profileValidator = $('#form-profile').validate({
+    rules: {
+      name: "required",
+      username: {
+        required: true,
+        notEqual: $('#form-profile input[name="username"]').attr('original'),
+        remote: {
+          url: '/validate',
+          type: 'post',
+          data: {
+            username: function() {
+              //console.log($('#form-register input[name="username"]').val());
+              return $('#form-profile input[name="username"]').val();
+            }
+          }
+        }
+      },
+      password: {
+          required: true,
+          pwcheck: true
+      },
+      email: {
+        email: true,
+        notEqual: $('#form-profile input[name="email"]').attr('original'),
+        remote: {
+          url: '/validate',
+          type: 'post',
+          data: {
+            email: function() {
+              //console.log($('#form-register input[name="username"]').val());
+              return $('#form-register input[name="email"]').val();
+            }
+          }
+        }
+      },
+      phoneNumber: {
+        phoneUS: true,
+        notEqual: $('#form-profile input[name="phoneNumber"]').attr('original'),
+        remote: {
+          url: '/validate',
+          type: 'post',
+          data: {
+            phoneNumber: function() {
+              //console.log($('#form-register input[name="username"]').val());
+              return $('#form-register input[name="phoneNumber"]').val();
+            }
+          }
+        }
+      }
+    },
+    messages: {
+      name: "A name is required.",
+      password: "Please enter a valid password.",
+      username: {
+        notEqual: "This is you!",
+        remote: "This username is taken.",
+      },
+      //username: "User exists already.",
+      email: {
+        notEqual: "This is you!",
+        remote: "Someone else has registered this email.",
+      },
+      phoneNumber: {
+        notEqual: "This is you!",
+        remote: "This phone number is registered in another account.",
+      },
     }
   });
 
