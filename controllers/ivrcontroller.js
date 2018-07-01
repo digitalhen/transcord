88bd8e4eb58048f1afe59b4123d8f7499620b806
Henry Williams <bitbucket@digitalhen.com>
2018-06-30 22:27:29 -0400

    refactors everything in to the ivrcontroller
    Former-commit-id: 14506093fccddc351d27a8f25ccf2c4fcdf197bc

---
+++ controllers/ivrcontroller.js
@@ -0,0 +1,191 @@
+const nodemailer = require('nodemailer');
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const User = require('../models/user');
+//const User = require('../models/user
+
+var ivrController = {};
+
+
+function reject() {
+	const voiceResponse = new VoiceResponse();
+
+	voiceResponse.say('Welcome to News Record. Your number was not recognized. Please visit news record dot com to register.');
+
+	voiceResponse.hangup();
+
+  return voiceResponse.toString();
+
+}
+
+ivrController.welcome = function(req, res) {
+
+	  const numberFrom = req.body.From;
+
+	  console.log("Incoming call from: " + numberFrom);
+
+		User.findOne({phoneNumber: numberFrom})
+	    .then(function(user) {
+	    	if(user==null) {
+					res.send(reject());
+	    	} else {
+	    		const name = user.name;
+					const voiceResponse = new VoiceResponse();
+
+					const gather = voiceResponse.gather({
+			                action: '/ivr/dialer',
+			                finishOnKey: '#',
+			                method: 'POST',
+			        });
+
+					gather.say("Hello " + name + ", please enter the number you wish to dial, followed by the # key.");
+
+			    res.send(voiceResponse.toString());
+	    	}
+	    })
+	    .catch(function(err) {
+	        console.log(err);
+	    });
+
+};
+
+ivrController.dialer = function(req, res) {
+	const numberFrom = req.body.Caller;
+	const numberCalled = req.body.Digits;
+
+	const voiceResponse = new VoiceResponse();
+
+	voiceResponse.say("Connecting you now.");
+
+	const dial = voiceResponse.dial({
+		record: 'record-from-ringing-dual',
+		recordingStatusCallback: '/ivr/recording?numberFrom=' + encodeURIComponent(numberFrom) + '&numberCalled=' + encodeURIComponent(numberCalled),
+		method: 'POST'
+	});
+
+	dial.number({
+		url: '/ivr/privacynotice',
+		method: 'POST'
+	}, numberCalled);
+
+	res.send(voiceResponse.toString());
+};
+
+ivrController.privacynotice = function(req, res) {
+	const numberFrom = req.body.Caller;
+
+	User.findOne({phoneNumber: numberFrom})
+		.then(function(user) {
+			if(user==null) {
+				// TODO: we should never reach here?
+			} else {
+				const name = user.name;
+
+				const voiceResponse = new VoiceResponse();
+
+				const gather = voiceResponse.gather({
+					action: '/ivr/privacyconnect',
+					numDigits: '1',
+					method: 'POST',
+				});
+
+				gather.say("You have an incoming call from " + name + ". Please press any key to accept.");
+
+				voiceResponse.say("Goodbye.");
+
+				voiceResponse.hangup();
+
+				res.send(voiceResponse.toString());
+			}
+		})
+		.catch(function(err) {
+				console.log(err);
+		});
+};
+
+ivrController.privacyconnect = function(req, res) {
+	const voiceResponse = new VoiceResponse();
+
+	voiceResponse.say("Connecting you to the call.");
+
+	res.send(voiceResponse.toString());
+}
+
+ivrController.recording = function(req, res) {
+	const numberFrom = decodeURIComponent(req.query.numberFrom);
+	const numberCalled = decodeURIComponent(req.query.numberCalled);
+	const recordingUrl = req.body.RecordingUrl;
+
+  User.findOne({phoneNumber: numberFrom})
+    .then(function(user) {
+    	if(user==null) {
+    		// TODO: we should never reach here?
+    	} else {
+        const name = user.name;
+        const email = user.email;
+
+				sendEmail(name, emailAddress, numberCalled, recordingUrl);
+
+				res.send('');
+    	}
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+}
+
+/**
+ * Returns an xml with the redirect
+ * @return {String}
+ */
+function redirectWelcome() {
+  const twiml = new VoiceResponse();
+
+  twiml.say('Returning to the main menu', {
+    voice: 'alice',
+    language: 'en-GB',
+  });
+
+  twiml.redirect('/welcome');
+
+  return twiml.toString();
+}
+
+function sendEmail(name, emailTo, numberCalled, recordingUrl) {
+	// create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        /*host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'digitalhen@gmail.com', // generated ethereal user
+            pass: 'iybfrzmemnvkyzhl' // generated ethereal password
+        }*/
+	host: 'localhost',
+	port: 25,
+	secure: false,
+	ignoreTLS: true,
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Transcord.app" <no-reply@transcord.app>', // sender address
+        to: emailTo, // list of receivers
+        subject: 'Recording of your call to ' + numberCalled, // Subject line
+        text: 'Dear ' + name + ',\n\nHere is your call recording: ' + recordingUrl, // plain text body
+        html: 'Dear ' + name + ',<br/><br/><b>Thank you for using News Recorder!</b><br/>' +
+		'The call recording can be found here: ' + recordingUrl
+// html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    });
+}
+
+module.exports = ivrController;
--- routes/ivr.js
+++ routes/ivr.js
@@ -1,6 +1,8 @@
 const Router = require('express').Router;
 const User = require('../models/user');
-const {reject, welcome, dialer, privacynotice, privacyconnect, recording} = require('../controllers/ivr');
+const ivr = require('../controllers/ivrcontroller');
+
+//const {reject, welcome, dialer, privacynotice, privacyconnect, recording} = require('../controllers/ivr');
 
 const router = new Router();
 
@@ -9,98 +11,18 @@ router.get('*', (req, res) => {
 });
 
 // POST: /ivr/welcome
-router.post('/welcome', (req, res) => {
-
-  const numberFrom = req.body.From;
-
-  console.log("Incoming call from: " + numberFrom);
-
-
-  //return res.send(welcome(callFrom));
-
-  User.findOne({phoneNumber: numberFrom})
-    .then(function(user) {
-    	if(user==null) {
-    		return res.send(reject());
-    	} else {
-    		const name = user.name;
-
-    		return res.send(welcome(name, numberFrom));
-    	}
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
-
-});
+router.post('/welcome', ivr.welcome);
 
 // POST: /ivr/dialer
-router.post('/dialer', (req, res) => {
-  const numberFrom = req.body.Caller;
-	const numberCalled = req.body.Digits;
-	return res.send(dialer(numberFrom, numberCalled));
-});
+router.post('/dialer', ivr.dialer);
 
 // POST: /ivr/privacynotice
-router.post('/privacynotice', (req, res) => {
-    const numberFrom = req.body.Caller;
+router.post('/privacynotice', ivr.privacynotice);
 
-    User.findOne({phoneNumber: numberFrom})
-      .then(function(user) {
-        if(user==null) {
-          // TODO: we should never reach here?
-        } else {
-          const name = user.name;
-
-          return res.send(privacynotice(name));
-        }
-      })
-      .catch(function(err) {
-          console.log(err);
-      });
-
-
-});
-
-// POST: /ivr/dialer
-router.post('/privacyconnect', (req, res) => {
-        return res.send(privacyconnect());
-});
+// POST: /ivr/privacyconnect
+router.post('/privacyconnect', ivr.privacyconnect);
 
 // POST: /ivr/recording
-router.post('/recording', (req, res) => {
-  const numberFrom = decodeURIComponent(req.query.numberFrom);
-	const numberCalled = decodeURIComponent(req.query.numberCalled);
-	const recordingUrl = req.body.RecordingUrl;
-
-  User.findOne({phoneNumber: numberFrom})
-    .then(function(user) {
-    	if(user==null) {
-    		// TODO: we should never reach here?
-    	} else {
-        const name = user.name;
-        const email = user.email;
-
-    		return res.send(recording(name, email, numberFrom, numberCalled, recordingUrl));
-    	}
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
-});
-
-/*
-// POST: /ivr/menu
-router.post('/menu', (req, res) => {
-  const digit = req.body.Digits;
-  return res.send(menu(digit));
-}); */
-
-/*
-// POST: /ivr/planets
-router.post('/planets', (req, res) => {
-  const digit = req.body.Digits;
-  res.send(planets(digit));
-}); */
+router.post('/recording', ivr.recording);
 
 module.exports = router;
