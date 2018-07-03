f3e8cfb052adf662983062b4c83e33ec6992625b
Henry Williams <bitbucket@digitalhen.com>
2018-07-03 14:34:30 -0400

    Running highlighting of messages tied to playback
    Former-commit-id: 6d105375675d085740f5962bc32c8fa0a704def0

--- public/js/app.js
+++ public/js/app.js
@@ -205,4 +205,29 @@ $(document).ready(function(){
           $('#form-register button[type="submit"]').prop('disabled', 'disabled');
       }
   });
+
+  // time tracker for audio
+  $('audio.transcriptAudioPlayer').bind('timeupdate', function() {
+    var currentTime = this.currentTime;
+    var found = null;
+
+    // dim everything first
+    $('.messageContainer').addClass('dimmed');
+
+    // search for the last message that has been passed by the audio
+    $('.messageContainer').each(function(index) {
+      var time = parseFloat($(this).attr('time'));
+      if (time < currentTime) {
+        found = $(this);
+      } else if (found !== null) {
+        // We've gone too far, so let's set the previous one to be highlighted, and then quit
+        found.removeClass('dimmed');
+        return false;
+      }
+    });
+
+
+
+
+  });
 });
