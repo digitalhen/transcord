97512f051ad42f37933e65fb8f86f5dc93ac4bd5
Henry Williams <bitbucket@digitalhen.com>
2018-07-03 10:54:59 -0400

    Adds mongo cloud to master connection helper
    Former-commit-id: 6410714f1aa36d1ac2ed6c3523eb87a96b816b98

--- helpers/connectionHelper.js
+++ helpers/connectionHelper.js
@@ -5,15 +5,20 @@ const mongoose = require('mongoose');
 mongoose.Promise = Promise;
 
 // Localhost
+/*
 exports.mongoConnection = mongoose.connect('mongodb://localhost/newsrecorder')
 	.then(() => console.log("Database connected successfully"))
-	.catch((err) => console.error(err));;
+	.catch((err) => console.error(err));
+*/
+
+
+// Mongo cloud
+exports.mongoConnection = mongoose.connect('mongodb+srv://testuser:testuser@cluster0-q3mk3.mongodb.net/test?retryWrites=true')
+	.then(() => console.log("Database connected successfully"))
+	.catch((err) => console.error(err));
 
 // Azure
 //exports.mongoConnection = mongoose.connect('mongodb://digitalhen:7q8URv8Vz6Ht5RgMRt8Y9y0H8AS7sHvtFxtZuvtmjUvObuZjjqy7oUXL6bVgohWdoW7myZRWS5iOVkUcFezbgw%3D%3D@digitalhen.documents.azure.com:10255/newsrecorder?ssl=true');
 
 // DigitalOcean
 //exports.mongoConnection = mongoose.connect('mongodb+srv://testuser:testuser@cluster0-q3mk3.mongodb.net/newsrecorder?retryWrites=true');
-
-
-
