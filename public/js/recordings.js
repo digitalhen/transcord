387cb8e3e18c77c697c700ee074a54ead3e5530b
Henry Williams <bitbucket@digitalhen.com>
2018-07-18 11:18:57 -0400

    gets transcript playing and syncing in browser.
    Former-commit-id: 2a7d993f23a89eb209524e74a8ce06ad8af3fe8a

--- public/js/recordings.js
+++ public/js/recordings.js
@@ -25,6 +25,28 @@ $(document).ready(function(){
     wavesurfer.on('finish', function (percents) {
       $(buttonTextId).text('play_arrow').removeClass('pause').addClass('play');
       wavesurfer.seekTo(0);
+      $('.messageContainer').removeClass('dimmed');
+    });
+
+    wavesurfer.on('audioprocess', function (percents) {
+      var currentTime = wavesurfer.getCurrentTime();
+      var found = null;
+
+      // dim everything first
+      $('.messageContainer').addClass('dimmed');
+
+      // search for the last message that has been passed by the audio
+      $('.messageContainer').each(function(index) {
+        var time = parseFloat($(this).attr('time'));
+        console.log("currentTime: " + currentTime + " time: " + time);
+        if (time < currentTime) {
+          found = $(this);
+        } else if (found !== null) {
+          // We've gone too far, so let's set the previous one to be highlighted, and then quit
+          found.removeClass('dimmed');
+          return false;
+        }
+      });
     });
 
     $(buttonId).click(function() {
@@ -34,8 +56,11 @@ $(document).ready(function(){
       // change icon
       if($(buttonTextId).hasClass('play'))
         $(buttonTextId).text('pause').removeClass('play').addClass('pause');
-      else
+      else { // we're pausing. undim everything
+        $('.messageContainer').removeClass('dimmed');
         $(buttonTextId).text('play_arrow').removeClass('pause').addClass('play');
+      }
+        
     });
   });
 });
