4c48b5e9dbbb210857bf690a94a0e308ee045dc4
Henry Williams <bitbucket@digitalhen.com>
2018-08-12 17:46:17 -0400

    format fix
    Former-commit-id: b32072f9a44764ffd61aecb215fcb9d84fa5d006

--- controllers/ivrcontroller.js
+++ controllers/ivrcontroller.js
@@ -86,7 +86,7 @@ ivrController.welcome = function(req, res) {
                     method: 'POST',
                 });
 
-                gather.say("Hello " + user.name + ", your current balance is $" + user.balance/100 + ". Please enter the number you wish to dial, followed by the # key.");
+                gather.say("Hello " + user.name + ", your current balance is $" + (user.balance/100).toFixed(2) + ". Please enter the number you wish to dial, followed by the # key.");
 
                 res.send(voiceResponse.toString());
             }
