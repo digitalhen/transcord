49e5812da3bee50cae2eb543acf21bf1a0666270
Henry Williams <bitbucket@digitalhen.com>
2018-08-12 20:02:03 -0400

    Clears up seed data, and a new database dump. Adds in extra values in to rates
    Former-commit-id: efba6338f8fa017552c68497b143d0067992e5ca

--- seed/rates.js
+++
@@ -1,9 +0,0 @@
-db.dropDatabase();
-
-const rates = [
-  {rateCode: 'DEFAULT', availabilityExpDate: '12/31/2199', existingExpDate: '12/31/2199', description: 'Default rate code', costPerBlock: 10, blockLength: 60 },
-  {rateCode: 'LOCALNEWS2018', availabilityExpDate: '12/31/2018', existingExpDate: '12/31/2199', description: 'Local news half-price', costPerBlock: 5, blockLength: 60 },
-];
-
-db.rates.insert(rates);
-db.rates.find();
