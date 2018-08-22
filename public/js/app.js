9cf358b35b9a091c990d7db6511fd3279791a60a
Henry Williams <bitbucket@digitalhen.com>
2018-08-21 23:10:47 -0400

    Converts a link to javascript actions for iOS (but does it for all)
    Former-commit-id: 9267a42a4fb9f0db5791f3d175b35d1ccfa13f42

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
   $('#overlay').fadeOut();
+
+  // convert a links to javascript location calls for iOS web app
+  var a=document.getElementsByTagName("a");
+  for(var i=0;i<a.length;i++)
+  {
+      a[i].onclick=function()
+      {
+          window.location=this.getAttribute("href");
+          return false
+      }
+  }
 });
