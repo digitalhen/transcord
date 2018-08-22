14acfaff4285a7f389d2e941857d6560b55b963a
Henry Williams <bitbucket@digitalhen.com>
2018-08-21 20:09:26 -0400

    email improvements
    Former-commit-id: 976015febd7791bb705b31dcaee689a820e87950

--- helpers/emailHelper.js
+++ helpers/emailHelper.js
@@ -40,7 +40,7 @@ emailHelper.sendEmail = function(user, subject, plaintext, html) {
         if (error) {
             return console.log(error);
         }
-        console.log('Message sent: %s', info.messageId);
+        console.log('Message sent to: ' + user.email + ' with ID: ' + info.messageId);
 
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
     });
