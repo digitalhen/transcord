05eab8a12510b94951ebc77484cfd7aeb8185737
Henry Williams <bitbucket@digitalhen.com>
2018-09-25 15:05:52 -0400

    dynamic recording dashboard
    Former-commit-id: 6b74b454884d38dcc0f33a44e26382f62e7d4f64

--- public/js/recordings.js
+++ public/js/recordings.js
@@ -1,50 +1,38 @@
 $(document).ready(function(){
-  // handle the email sharing of transcripts
-  $('.transcript-control-card .email-button').click(function() {
-    var dialog = $('#sharetranscript-dialog')[0];
 
-    if (! dialog.showModal) {
-      dialogPolyfill.registerDialog(dialog);
+
+  // load initial
+  $.post("/dashboard/ajaxSearchRecordings", function(data) {
+    console.log(data);
+
+    // TODO: check page count here
+    // TODO: don't get transcripts here
+
+    for (var i=0; i < data.length; i++) {
+      var clone = $('#recording-card-template').clone().prop('id', 'recording-card-' + i).attr('data-iteration', i).show();
+
+      // TODO: update values here
+      clone.attr('data-recordingSid', data[i].recordingSid);
+      
+      if(data[i].callDirection==0)
+        clone.find('.call-made-button').show();
+      else if(data[i].callDirection==1)
+        clone.find('.call-received-button').show();
+
+      clone.find('audio').children('source').attr('src', data[i].recordingUrl);
+
+      if(!data[i].processingStatus || data[i].processingStatus==2) {
+        clone.find('.still-processing').remove();
+      } else {
+        clone.find('.done-processing').remove();
+      }
+
+
+
+      $('#recording-card-container').append(clone);
     }
-
-    dialog.showModal();
   });
-
-  $('#sharetranscript-dialog .cancel-button').click(function() {
-    var dialog = $('#sharetranscript-dialog')[0];
-
-    dialog.close();
-  });
-
-  $('#sharetranscript-dialog #form-sharetranscript').submit(function() {
-    var form = $(this);
-
-    if(form.find('.is-invalid').length > 0) {
-      // do nothing, because form is invalid
-    } else {
-      var recordingSid = $(this).attr('data-recordingSid');
-      var email = $(this).find('input[name="email"]').val();
-
-      $.post("/dashboard/ajaxSendTranscript", { 'email': email, 'recordingSid': recordingSid }, function( data ) {
-        console.log(data);
-
-        if(data.status==="Success") {
-          var dialog = $('#sharetranscript-dialog')[0];
-
-          dialog.close();
-
-          // TODO: pop up somehow?
-        } else if (data.status==="Error") {
-          console.log(data.status.message);
-        }
-      });
-
-    }
-    
-    return false;
-
-    /*  */
-  });
+  
 
   // handle the deletes here but only on the dashboard
   $('.recording-card .delete-button').click(function() {
@@ -78,95 +66,5 @@ $(document).ready(function(){
   });
 
 
-  // waveforms go on the transcript page
-  $('.waveform').each(function() {
-    var idNumber = $(this).attr('data-id');
-    var waveformId = '#waveform-' + idNumber;
-    var progressId = 'progress-' + idNumber;
-    var buttonId = '#button-' + idNumber;
-    var buttonTextId = '#button-text-' + idNumber;
-
-    var wavesurfer = WaveSurfer.create({
-      container: waveformId,
-      barWidth: 3,
-    });
-
-    wavesurfer.load($(this).attr('data-recordingUrl'));
-
-    wavesurfer.on('loading', function (percents) {
-      document.getElementById(progressId).value = percents;
-    });
-
-    wavesurfer.on('ready', function (percents) {
-      document.getElementById(progressId).style.display = 'none';
-    });
-
-    wavesurfer.on('finish', function (percents) {
-      $(buttonTextId).text('play_arrow').removeClass('pause').addClass('play');
-      wavesurfer.seekTo(0);
-      $('.messageContainer').removeClass('dimmed');
-    });
-
-    wavesurfer.on('audioprocess', function (percents) { 
-      var currentTime = wavesurfer.getCurrentTime();
-      var found = null;
-
-      //console.log("currentTime:" +  currentTime);
-      
-      // dim everything first
-      $('.word').removeClass('highlight');
-
-      // search for the last message that has been passed by the audio
-      
-      $('.word').filter(function() {
-        return parseFloat($(this).attr('data-startTime')) < currentTime;
-      }).filter(function() {
-        return parseFloat($(this).attr('data-endTime')) > currentTime;
-      })
-      .addClass('highlight');
-
-      /*$('.messageContainer').each(function(index) {
-
-        var time = parseFloat($(this).attr('data-time'));
-        console.log(currentTime + " vs " + time + ": " + (time<currentTime));
-        if (time < currentTime) {
-          // we've found a match but keep checking
-          found = $(this);
-
-          if(found[0] === $('.messageContainer').last()[0]) {
-            // special handling for the last one
-            found.removeClass('dimmed');
-            return false;
-          }
-        } else if (found !== null) {
-          // we've gone beyond where the audio is, so let's stop and mark the last good one as where we are
-          found.removeClass('dimmed');
-          return false;
-        }
-      }); */
-
-
-    });
-
-    $('.word').click(function() {
-      var duration = wavesurfer.getDuration();
-      var percentage = parseFloat($(this).attr('data-startTime')) / duration;
-
-      wavesurfer.seekTo(percentage);
-    });
-
-    $(buttonId).click(function() {
-      wavesurfer.playPause();
-
-
-      // change icon
-      if($(buttonTextId).hasClass('play'))
-        $(buttonTextId).text('pause').removeClass('play').addClass('pause');
-      else { // we're pausing. undim everything
-        $('.messageContainer').removeClass('dimmed');
-        $(buttonTextId).text('play_arrow').removeClass('pause').addClass('play');
-      }
-        
-    });
-  });
+  
 });
---
+++ public/js/transcript.js
@@ -0,0 +1,140 @@
+$(document).ready(function() {
+// handle the email sharing of transcripts
+    $('.transcript-control-card .email-button').click(function() {
+        var dialog = $('#sharetranscript-dialog')[0];
+
+        if (! dialog.showModal) {
+        dialogPolyfill.registerDialog(dialog);
+        }
+
+        dialog.showModal();
+    });
+
+    $('#sharetranscript-dialog .cancel-button').click(function() {
+        var dialog = $('#sharetranscript-dialog')[0];
+
+        dialog.close();
+    });
+
+    $('#sharetranscript-dialog #form-sharetranscript').submit(function() {
+        var form = $(this);
+
+        if(form.find('.is-invalid').length > 0) {
+        // do nothing, because form is invalid
+        } else {
+        var recordingSid = $(this).attr('data-recordingSid');
+        var email = $(this).find('input[name="email"]').val();
+
+        $.post("/dashboard/ajaxSendTranscript", { 'email': email, 'recordingSid': recordingSid }, function( data ) {
+            console.log(data);
+
+            if(data.status==="Success") {
+            var dialog = $('#sharetranscript-dialog')[0];
+
+            dialog.close();
+
+            // TODO: pop up somehow?
+            } else if (data.status==="Error") {
+            console.log(data.status.message);
+            }
+        });
+
+        }
+        
+        return false;
+
+        /*  */
+    });
+
+    // waveforms go on the transcript page
+  $('.waveform').each(function() {
+    var idNumber = $(this).attr('data-id');
+    var waveformId = '#waveform-' + idNumber;
+    var progressId = 'progress-' + idNumber;
+    var buttonId = '#button-' + idNumber;
+    var buttonTextId = '#button-text-' + idNumber;
+
+    var wavesurfer = WaveSurfer.create({
+      container: waveformId,
+      barWidth: 3,
+    });
+
+    wavesurfer.load($(this).attr('data-recordingUrl'));
+
+    wavesurfer.on('loading', function (percents) {
+      document.getElementById(progressId).value = percents;
+    });
+
+    wavesurfer.on('ready', function (percents) {
+      document.getElementById(progressId).style.display = 'none';
+    });
+
+    wavesurfer.on('finish', function (percents) {
+      $(buttonTextId).text('play_arrow').removeClass('pause').addClass('play');
+      wavesurfer.seekTo(0);
+      $('.messageContainer').removeClass('dimmed');
+    });
+
+    wavesurfer.on('audioprocess', function (percents) { 
+      var currentTime = wavesurfer.getCurrentTime();
+      var found = null;
+
+      //console.log("currentTime:" +  currentTime);
+      
+      // dim everything first
+      $('.word').removeClass('highlight');
+
+      // search for the last message that has been passed by the audio
+      
+      $('.word').filter(function() {
+        return parseFloat($(this).attr('data-startTime')) < currentTime;
+      }).filter(function() {
+        return parseFloat($(this).attr('data-endTime')) > currentTime;
+      })
+      .addClass('highlight');
+
+      /*$('.messageContainer').each(function(index) {
+
+        var time = parseFloat($(this).attr('data-time'));
+        console.log(currentTime + " vs " + time + ": " + (time<currentTime));
+        if (time < currentTime) {
+          // we've found a match but keep checking
+          found = $(this);
+
+          if(found[0] === $('.messageContainer').last()[0]) {
+            // special handling for the last one
+            found.removeClass('dimmed');
+            return false;
+          }
+        } else if (found !== null) {
+          // we've gone beyond where the audio is, so let's stop and mark the last good one as where we are
+          found.removeClass('dimmed');
+          return false;
+        }
+      }); */
+
+
+    });
+
+    $('.word').click(function() {
+      var duration = wavesurfer.getDuration();
+      var percentage = parseFloat($(this).attr('data-startTime')) / duration;
+
+      wavesurfer.seekTo(percentage);
+    });
+
+    $(buttonId).click(function() {
+      wavesurfer.playPause();
+
+
+      // change icon
+      if($(buttonTextId).hasClass('play'))
+        $(buttonTextId).text('pause').removeClass('play').addClass('pause');
+      else { // we're pausing. undim everything
+        $('.messageContainer').removeClass('dimmed');
+        $(buttonTextId).text('play_arrow').removeClass('pause').addClass('play');
+      }
+        
+    });
+  });
+});
